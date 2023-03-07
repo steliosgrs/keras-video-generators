de98ac49a0d780c5ca62001f58a01888df3539c7
Stelios Georgaras <steliosgp13@gmail.com>
2023-03-07 12:37:42 +0200

    frame cache problem fixed

--- src/keras_video/generator.py
+++ src/keras_video/generator.py
@@ -353,7 +353,8 @@ class VideoFrameGenerator(Sequence):
                     continue
 
                 # add to cache
-                self.__frame_cache[video] = frames
+                if self.use_frame_cache:
+                    self.__frame_cache[video] = frames
 
             else:
                 frames = self.__frame_cache[video]
